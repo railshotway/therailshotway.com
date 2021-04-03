d6a9c609d20575fcdc12f7bac88de0a0b5568d48
Jose Farias <jose@farias.mx>
2021-04-03 12:30:03 -0600

    Make posts layout singular

--- config.rb
+++ config.rb
@@ -30,13 +30,12 @@ page '/', layout: 'default'
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   blog.prefix = 'posts'
-  blog.layout = 'posts'
+  blog.layout = 'post'
 
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
   # blog.sources = "{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "post"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = "{year}.html"
